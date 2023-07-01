b782b6f57fa0225218662a153a7b5988192cbea2
Julius Jeske <s_jeskej21@stud.hwr-berlin.de>
2023-07-01 18:58:46 +0200

    nochmal fehler geloescht

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -5,7 +5,7 @@ CREATE TABLE kathi (
 );
 CREATE TABLE wohnungsinersat (
 	inserat_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    FOREIGN KEY (nutzer_id) REFERENCES kathi (nutzer_id) ON DELETE CASCADE,
+    nutzer_id INTEGER, FOREIGN KEY (nutzer_id) REFERENCES kathi (nutzer_id) ON DELETE CASCADE,
 	name TEXT NOT NULL,
     barrierefreiheit BOOLEAN,
     balkonvorhanden BOOLEAN,
@@ -21,15 +21,15 @@ CREATE TABLE wohnungsinersat (
 );
 CREATE TABLE foreneintrag (
     forumseintags_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nutzer_id FOREIGN KEY (nutzer_id) REFERENCES kathi (nutzer_id) ON DELETE CASCADE,
+    nutzer_id INTEGER, FOREIGN KEY (nutzer_id) REFERENCES kathi (nutzer_id) ON DELETE CASCADE,
     inhalt Text 
 	
 );
 
 CREATE TABLE inserat_erstellt_von(
     PRIMARY KEY( nutzer_id, inserat_id),
-    nutzer_id FOREIGN KEY (nutzer_id) REFERENCES kathi (nutzer_id) ON DELETE CASCADE,
-    inserat_id FOREIGN KEY (inserat_id) REFERENCES wohnungsinserat (inserat_id) ON DELETE CASCADE
+    nutzer_id INTEGER, FOREIGN KEY (nutzer_id) REFERENCES kathi (nutzer_id) ON DELETE CASCADE,
+    inserat_id INTEGER, FOREIGN KEY (inserat_id) REFERENCES wohnungsinserat (inserat_id) ON DELETE CASCADE
 );
 
 
