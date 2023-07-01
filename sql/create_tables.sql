ee43821351ea5a10d637cb740c461d2bd9e071a6
Julius Jeske <s_jeskej21@stud.hwr-berlin.de>
2023-07-01 18:58:06 +0200

    fixed Errors in foreign key deklaration

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -14,20 +14,22 @@ CREATE TABLE wohnungsinersat (
     egage INTEGER,
     zimmeranz INTEGER,
     quadratmeter INTEGER, 
-    ort TEXT
+    ort TEXT,
+    kommentar TEXT
+
 
 );
 CREATE TABLE foreneintrag (
-	nutzer_id INTEGER,
     forumseintags_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nutzer_id FOREIGN KEY (nutzer_id) REFERENCES kathi (nutzer_id) ON DELETE CASCADE,
     inhalt Text 
 	
 );
 
-CREATE TABLE kathis_eintrag(
+CREATE TABLE inserat_erstellt_von(
     PRIMARY KEY( nutzer_id, inserat_id),
-    FOREIGN KEY (nutzer_id) REFERENCES kathi (nutzer_id) ON DELETE CASCADE,
-    FOREIGN KEY (inserat_id) REFERENCES wohnungsinserat (inserat_id) ON DELETE CASCADE
+    nutzer_id FOREIGN KEY (nutzer_id) REFERENCES kathi (nutzer_id) ON DELETE CASCADE,
+    inserat_id FOREIGN KEY (inserat_id) REFERENCES wohnungsinserat (inserat_id) ON DELETE CASCADE
 );
 
 
